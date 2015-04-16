80e5064900e6e9a612759faa68e7aafbbeee63ca
Daria <dasham94@yandex.ru>
2015-04-16 14:06:50 +0300

    casting of grandparents with demonstration\impl with bad memory keeping (without recursive)

--- RTTI/main.cpp
+++ RTTI/main.cpp
@@ -14,12 +14,16 @@ public:
 
 	template <typename P, typename C>
 	void addParent(){
-		std::cout << "addP" << std::endl;
+	//	std::cout << "addP" << std::endl;
 		P p;
 		C c = C(0);
 		parent.push_back(p.getInfo());
 		offset.push_back((size_t)(static_cast<P*>(&c)) - (size_t)(&c));
-		std::cout << "addPEnd" << std::endl;
+		for (int i = 0; i < p.getInfo().parent.size(); ++i) {
+			parent.push_back(p.getInfo().parent[i]);
+			offset.push_back((size_t)(static_cast<P*>(&c)) - (size_t)(&c) + p.getInfo().offset[i]);
+		}
+	//	std::cout << "addPEnd" << std::endl;
 	}
 	my_type_info(){ ; }
 	my_type_info(std::string name) :classN(name){ ; }
@@ -38,7 +42,6 @@ public:
 		return info;\
 	}\
 	className(){ \
-		std::cout<<#className<<std::endl;\
 			info.classN = #className;\
 
 #define RTTI_CONSTRUCT_END(className)}\
@@ -54,8 +57,9 @@ targetClass* casting(sourseClass* soursePtr) {
 	size_t targetClassHash = tC.getInfo().getClassHash();
 	size_t offsetUp = 0;
 	size_t offsetDown = 0;
+	size_t deep = 0;
 	for (int i = 0; i < soursePtr->getVirtualInfo().parent.size(); ++i) {
-		std::cout << "%" << soursePtr->getVirtualInfo().parent[i].classN << std::endl;
+	//	std::cout << "%" << soursePtr->getVirtualInfo().parent[i].classN << std::endl;
 		if (soursePtr->getVirtualInfo().parent[i].getClassHash() == targetClassHash) {
 			offsetUp = soursePtr->getVirtualInfo().offset[i];
 		}
@@ -67,16 +71,35 @@ targetClass* casting(sourseClass* soursePtr) {
 	return reinterpret_cast<targetClass*>((void *)((size_t)soursePtr - offsetDown + offsetUp));
 }
 
-
 #define DYNAMIC_CAST(targetClass, sourseClass, soursePtr) casting<targetClass, sourseClass>(soursePtr);
 	
-					
+class Y {
+	RTTI_CONSTRUCT(Y)
+	RTTI_CONSTRUCT_END(Y)
+public:
+	char y = 'y';
+	void helloY() {
+		std::cout << "I am Y!" << std::endl;
+	}
+};
 
-class A {
+class Z {
+	RTTI_CONSTRUCT(Z)
+	RTTI_CONSTRUCT_END(Z)
+public:
+	char z = 'z';
+	void helloZ() {
+		std::cout << "I am Z!" << std::endl;
+	}
+};
+
+class A: public Z, public Y {
 	RTTI_CONSTRUCT(A)
+	DECLARE_PARENT(A, Z);
+	DECLARE_PARENT(A, Y);
 	RTTI_CONSTRUCT_END(A)
 public:
-	int a = 1;
+	char a = 'a';
 	void helloA() {
 		std::cout << "I am A!" << std::endl;
 	}
@@ -87,7 +110,7 @@ class C {
 	RTTI_CONSTRUCT(C)
 	RTTI_CONSTRUCT_END(C)
 public:
-	int c = 2;
+	char c = 'c';
 	void helloC() {
 		std::cout << "I am C! My field c = " << this->c << std::endl;
 	}
@@ -112,14 +135,26 @@ int main() {
 	A* a1 = new A();
 	//пытаемся связать два базовых класса без объекта-наследника - 
 	//работает обычный reinterpret_cast, поле "с" отождествляется с полем "a" 
+	std::cout << "base to base without derived object" << std::endl;
 	c = DYNAMIC_CAST(C, A, a1);
 	c->helloC();
-	std::cout << c->c;
 	//пытаемся связать два базовых класса через объект-наследник - поле "a" уходит из области видимости,
 	//поле "c" появляется и не отождествляется с полем "a"
+	std::cout << "base to base with derived object" << std::endl;
 	c = DYNAMIC_CAST(C, A, a);
 	c->helloC();
-	std::cout << c->c;
+
+	//кастуем "дедушку мамы к папе" без внука
+	std::cout << "base to base of another base without derived object" << std::endl;
+	Y* y1 = new Y();
+	c = DYNAMIC_CAST(C, Y, y1);
+	c->helloC();
+
+	//кастуем "дедушку мамы к папе" через внука
+	std::cout << "base to base of another base with derived object" << std::endl;
+	Y* y = new B();
+	c = DYNAMIC_CAST(C, Y, y);
+	c->helloC();
 	int n;
 	std::cin >> n;
 	return 0;
